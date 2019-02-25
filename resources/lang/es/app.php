e83215508b1ddf564bfcafa7ae80e4a43bda1772
Johnssther <johnssther@gmail.com>
2019-02-24 22:22:23 -0500

    route inicio ok

---
+++ resources/lang/es/app.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | App Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during all app KOI-TI
+    |
+    */
+
+	'login' => 'Iniciar sesión',
+    'logout' => 'Cerrar sesión',
+
+    'exception' => 'Ha ocurrido un error inesperado por favor recarga tu navegador.',
+
+    'home' => 'Inicio',
+
+    'create' => 'Crear',
+    'save' => 'Guardar',
+    'edit' => 'Editar',
+    'comeback' => 'Regresar',
+    'cancel' => 'Cancelar',
+    'add' => 'Agregar',
+    'find' => 'Buscar',
+    'continue' => 'Continuar',
+    'clear' => 'Limpiar',
+	'delete' => 'Eliminar',
+
+    'pdf' => 'Generar PDF',
+    'xls' => 'Generar XLS',
+
+	'import' => [
+		'xls' => 'Importar XLS',
+	],
+
+	'files' => [
+        'choose-file' => 'Examinar...',
+        'drop' => 'Arrastre y suelte los archivos aquí...',
+        'retry' => 'Volver a intentar',
+        'process' => 'Procesando los archivos...'
+    ]
+];
---
+++ resources/lang/es/notification.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | App Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during all app KOI-TI
+    |
+    */
+
+	'view' => 'Ver todo',
+
+	'call' => [
+        'pending' => 'Tiene una llamada pendiente',
+        'gestioncobro' => 'Tiene una gestión de cobro pendiente',
+        'gestiondeudor' => 'Tiene una gestión de deudor pendiente'
+    ]
+];
