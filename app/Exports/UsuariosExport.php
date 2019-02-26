b92f679d0a064ce9113c681f36347237e89f77ee
Johnssther <johnssther@gmail.com>
2019-02-25 21:32:39 -0500

    ->orderBy('age', 'asc')->get(); ordenando la edad de forma asendente

--- app/Exports/UsuariosExport.php
+++ app/Exports/UsuariosExport.php
@@ -6,6 +6,8 @@ use App\Models\Usuario;
 
 use App\Invoice;
 
+use DB;
+
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
@@ -15,8 +17,16 @@ class UsuariosExport implements FromView, ShouldAutoSize
 {
     public function view(): View
     {
+
+         $usuarios = DB::table('usuarios')->orderBy('age', 'asc')->get();
+ 
         return view('excel_generate.excel_usuarios', [
-            'usuarios' => Usuario::all()
+            'usuarios' => $usuarios
+            
         ]);
     }
 }
+
+/*   return view('excel_generate.excel_usuarios', [
+            'usuarios' => Usuario::get()->orderBy('name', 'desc')
+            ]); */
